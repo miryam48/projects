44cce6f83be9fd90789d0b3434b283138e1eca27
FLUTTER <miryamsam48@gmail.com>
2024-02-15 00:03:56 -0800

    ok

--- lib/signin.dart
+++ lib/signin.dart
@@ -70,6 +70,7 @@ class _signinState extends State<signin> {
                   ),
                     onPressed: (){
                     signIn();
+                    Navigator.pop(context);
                     },
                     child: Text('Sign In')
                 )
